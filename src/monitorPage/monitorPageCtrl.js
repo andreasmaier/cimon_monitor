1d792bfcd444161d398effc56bfbb7f01b679fdb
andreasmaier <andreas.maier@gmail.com>
2016-05-15 22:13:16 -0700

    get basic setup for jobs

--- src/monitorPage/monitorPageCtrl.js
+++ src/monitorPage/monitorPageCtrl.js
@@ -1,5 +1,8 @@
 angular.module('cimonmon').controller('MonitorPageCtrl', function ($scope, $rootScope, $log) {
-    $scope.testString = "foo";
+    $scope.testString1 = "my first job";
+    $scope.testString2 = "the other job";
+    $scope.testString3 = "job with a long description that goes on and on";
+    $scope.testString4 = "yet another job";
     $scope.monitorData = [];
 
     $rootScope.$on('jobStatusUpdate', function (event, args) {
